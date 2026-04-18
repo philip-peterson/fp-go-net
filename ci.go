277f09b7a64bf6e7de2b1089078ceacda4070fc9
Philip Peterson <1326208+philip-peterson@users.noreply.github.com>
2026-04-18 01:59:20 -0700

    Refine dagger tests

--- ci.go
+++ ci.go
@@ -7,11 +7,30 @@ import (
 
 type Repo struct{}
 
-func (r *Repo) Test(ctx context.Context) error {
+func (r *Repo) BuildNet(ctx context.Context) error {
+	return r.build(ctx, "fp-go-net")
+}
+
+func (r *Repo) BuildTLS(ctx context.Context) error {
+	return r.build(ctx, "fp-go-net-tls")
+}
+
+func (r *Repo) build(ctx context.Context, path string) error {
 	_, err := dag.Container().
 		From("golang:1.22").
 		WithDirectory("/repo", dag.Host().Directory(".")).
-		WithWorkdir("/repo").
+		WithWorkdir("/repo/" + path).
+		WithExec([]string{"go", "build", "./..."}).
+		Sync(ctx)
+
+	return err
+}
+
+func (r *Repo) TestIRC(ctx context.Context) error {
+	_, err := dag.Container().
+		From("golang:1.22").
+		WithDirectory("/repo", dag.Host().Directory(".")).
+		WithWorkdir("/repo/examples/ircserver").
 		WithExec([]string{"go", "test", "./..."}).
 		Sync(ctx)
 
